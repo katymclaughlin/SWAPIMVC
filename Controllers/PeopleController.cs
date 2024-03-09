67fa8973b9a7cc26ff9f6d417986a977733f26ce
Katy McLaughlin <mclaughlinkaty6@gmail.com>
2024-03-09 14:29:04 -0500

    "Created HttpClient"

--- Controllers/PeopleController.cs
+++ Controllers/PeopleController.cs
@@ -8,6 +8,11 @@ namespace SwapiMVC.Controllers
 {
     public class PeopleController : Controller
     {
+        private readonly HttpClient _httpClient;
+        public PeopleController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClient = httpClientFactory.CreateClient("swapi");
+        }
         public IActionResult Index()
         {
             return View();
